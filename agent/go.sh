2b89b966f52f684f5ba1592c41aaa39bd705c757
shehzad-kore <sibrahim@korewireless.com>
2023-01-06 14:36:48 +0530

    update

--- agent/go.sh
+++ agent/go.sh
@@ -1,7 +1,5 @@
 #!/bin/bash -e
 
-export PYTHONPATH=$PWD/gencode/python
 
-cd udmi_site_model
 
-python udmi_agent.py  --registry_id=NuclearReactor   --subscription_id=korewireless-development  --device_id=NuclearSensor01  --algorithm=RS256  --private_key_file=device1.key --ca_certs=omnicore_ca.pem --mqtt_bridge_hostname=korewireless-development.mqtt.korewireless.com
+python udmi_agent.py  --registry_id=NuclearReactor   --subscription_id=korewireless-development  --device_id=NuclearSensor01  --algorithm=RS256  --private_key_file=keys/device1.key --ca_certs=keys/omnicore_ca.pem --mqtt_bridge_hostname=korewireless-development.mqtt.korewireless.com
