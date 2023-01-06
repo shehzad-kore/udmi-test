317a3a501fbad06731f595dd0f51d89471caa15d
shehzad-kore <sibrahim@korewireless.com>
2023-01-06 12:24:32 +0530

    Update sh

--- agent/go.sh
+++ agent/go.sh
@@ -4,6 +4,4 @@ export PYTHONPATH=$PWD/gencode/python
 
 cd udmi_site_model
 
-../venv/bin/python3 ../agent/udmi_agent.py \
-                    --algorithm RS256 --private_key_file devices/AHU-1/rsa_private.pem --ca_certs ../roots.pem \
-                    --device_id AHU-1 --registry_id ZZ-TRI-FECTA --project bos-daq-testing
+python udmi_agent.py  --registry_id=NuclearReactor   --subscription_id=korewireless-development  --device_id=NuclearSensor01  --algorithm=RS256  --private_key_file=device1.key --ca_certs=omnicore_ca.pem --mqtt_bridge_hostname=korewireless-development.mqtt.korewireless.com
